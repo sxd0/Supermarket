0bd7bc9131f713fd9f23913129fe48878cd74502
sxd0 <fancityfun@gmail.com>
2024-11-29 22:42:08 +0500

    feat: Add New Routers

--- backend/app/card/router.py
+++ backend/app/card/router.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.card.dao import CardDAO
+from app.card.models import Card
+from app.user.dependencies import get_current_user
+from app.user.models import User
 
 
 router = APIRouter(
@@ -10,13 +13,20 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_card():
+async def get_card(): # Получение всех товаров
     return await CardDAO.find_all()
 
-
-@router.get("/{id}")
+@router.get("/{card_id}") # Получени одного товара по id
 async def get_card_by_id(card_id: int):
     return await CardDAO.find_one_or_none(id = card_id)
+
+@router.get("/new/") # Получение всех новых товаров
+async def get_new_card():
+    return await CardDAO.find_all(new = True)
+
+@router.get("/only/{category_id}") # Получение всех товаров по категориям
+async def get_card_for_category(category_id: int):
+    return await CardDAO.find_all(category_id = category_id)
 
 """
 GET /cards
--- backend/app/review/router.py
+++ backend/app/review/router.py
@@ -1,10 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.review.dao import ReviewDAO
+from app.user.dependencies import get_current_user
+from app.user.models import User
 
 
 router = APIRouter(
     prefix="/review",
     tags=["Отзывы"],
 )
+
+# @router.get("") # Инструмент разработчика
+# async def get_reviews():
+#     return await ReviewDAO.find_all()
+
+@router.get("") # Получение всех отзывов для пользователя
+async def get_reviews_for_user(user: User = Depends(get_current_user)):
+    return await ReviewDAO.find_all(user_id=user.id)
+
+
+
+@router.get("/{card_id}") # Получение всех отзывов на товар
+async def get_reviews_card(card_id: int):
+    return await ReviewDAO.find_all(card_id = card_id)
 
 """
 POST /reviews
