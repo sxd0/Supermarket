88df5f7297f9347cad661e90f3d81d3177bd99c5
xAsssenov <aks.kirill32@gmail.com>
2024-12-02 19:22:08 -0800

    feat: add breadcrumbs with page about and fix structure

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,31 +9,33 @@ const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.header__container}>
+        <Link to="/" className={styles.header__button}>
         <p className={styles.header__title}>AllSad</p>
+        </Link>
 
         <section className={styles.header__section}>
-          <Link to="/home" className={styles.header__button}>
+          <Link to="/" className={styles.header__button}>
             <p>Женщинам</p>
           </Link>
-          <Link to="/home" className={styles.header__button}>
+          <Link to="/" className={styles.header__button}>
             <p>Мужчинам</p>
           </Link>
-          <Link to="/home" className={styles.header__button}>
+          <Link to="/new" className={styles.header__button}>
             <p>Новинки</p>
           </Link>
-          <Link to="/home" className={styles.header__button}>
+          <Link to="/about" className={styles.header__button}>
             <p>О нас</p>
           </Link>
         </section>
 
         <section className={styles.header__buttons}>
-          <Link to="/home">
+          <Link to="/">
             <ReactSVG src={favouriteIcon} className={styles.icon} />
           </Link>
-          <Link to="/home">
+          <Link to="/">
             <ReactSVG src={profileIcon} className={styles.icon} />
           </Link>
-          <Link to="/home">
+          <Link to="/cart">
             <ReactSVG src={cartIcon} className={styles.icon} />
           </Link>
         </section>
