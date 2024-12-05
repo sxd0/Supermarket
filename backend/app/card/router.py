68e613d5b477ada88e22153dd8f77697fc7aab73
sxd0 <fancityfun@gmail.com>
2024-12-05 19:12:57 +0500

    fix: Fix Docker and Routing card/new

--- backend/app/card/router.py
+++ backend/app/card/router.py
@@ -16,13 +16,14 @@ router = APIRouter(
 async def get_card(): # Получение всех товаров
     return await CardDAO.find_all()
 
+@router.get("/new") # Получение всех новых товаров
+async def get_new_card():
+    return await CardDAO.find_all(new = True)
+
 @router.get("/{card_id}") # Получени одного товара по id
 async def get_card_by_id(card_id: int):
     return await CardDAO.find_one_or_none(id = card_id)
 
-@router.get("/new/") # Получение всех новых товаров
-async def get_new_card():
-    return await CardDAO.find_all(new = True)
 
 @router.get("/only/{category_id}") # Получение всех товаров по категориям
 async def get_card_for_category(category_id: int):
--- backend/seed_data.sql
+++ backend/seed_data.sql
@@ -4,15 +4,16 @@ TRUNCATE TABLE "user" RESTART IDENTITY CASCADE;
 TRUNCATE TABLE card RESTART IDENTITY CASCADE;
 TRUNCATE TABLE cart RESTART IDENTITY CASCADE;
 TRUNCATE TABLE review RESTART IDENTITY CASCADE;
+TRUNCATE TABLE alembic_version RESTART IDENTITY CASCADE;
 
 -- Вставка данных в таблицу category
 INSERT INTO category (id, title, gender, image) VALUES
-(1, 'T-Shirts', 'Male', 1),
-(2, 'Dresses', 'Female', 2),
-(3, 'Jeans', 'Unisex', 3),
-(4, 'Jackets', 'Male', 4),
-(5, 'Shoes', 'Female', 5),
-(6, 'Accessories', 'Unisex', 6);
+(1, 'T-Shirts', 'Male', 'image1.jpg'),
+(2, 'Dresses', 'Female', 'image2.jpg'),
+(3, 'Jeans', 'Unisex', 'image3.jpg'),
+(4, 'Jackets', 'Male', 'image4.jpg'),
+(5, 'Shoes', 'Female', 'image5.jpg'),
+(6, 'Accessories', 'Unisex', 'image6.jpg');
 
 -- Вставка данных в таблицу user
 INSERT INTO "user" (id, email, hashed_password, name, surname) VALUES
@@ -23,13 +24,13 @@ INSERT INTO "user" (id, email, hashed_password, name, surname) VALUES
 (5, 'eve@example.com', 'hashed_password_5', 'Eve', 'Adams');
 
 -- Вставка данных в таблицу card
-INSERT INTO card (id, title, price, category_id, quantity, description, sale, new, flag, image) VALUES
-(1, 'Cotton T-Shirt', 20, 1, 50, 'A comfortable cotton T-Shirt', NULL, TRUE, FALSE, 1),
-(2, 'Summer Dress', 40, 2, 30, 'A stylish summer dress', 5, TRUE, TRUE, 2),
-(3, 'Blue Jeans', 35, 3, 40, 'Classic blue jeans', NULL, FALSE, FALSE, 3),
-(4, 'Leather Jacket', 80, 4, 20, 'A durable leather jacket', 10, FALSE, TRUE, 4),
-(5, 'High Heels', 60, 5, 25, 'Elegant high heels', NULL, TRUE, FALSE, 5),
-(6, 'Winter Scarf', 15, 6, 60, 'Warm winter scarf', NULL, FALSE, FALSE, 6);
+INSERT INTO card (id, title, price, category_id, quantity, description, sale, new, flag, image, size) VALUES
+(1, 'Cotton T-Shirt', 20, 1, 50, 'A comfortable cotton T-Shirt', NULL, TRUE, FALSE, 'image1.jpg', 'M'),
+(2, 'Summer Dress', 40, 2, 30, 'A stylish summer dress', 5, TRUE, TRUE, 'image2.jpg', 'S'),
+(3, 'Blue Jeans', 35, 3, 40, 'Classic blue jeans', NULL, FALSE, FALSE, 'image3.jpg', 'L'),
+(4, 'Leather Jacket', 80, 4, 20, 'A durable leather jacket', 10, FALSE, TRUE, 'image4.jpg', 'XL'),
+(5, 'High Heels', 60, 5, 25, 'Elegant high heels', NULL, TRUE, FALSE, 'image5.jpg', 'M'),
+(6, 'Winter Scarf', 15, 6, 60, 'Warm winter scarf', NULL, FALSE, FALSE, 'image6.jpg', 'One Size');
 
 -- Вставка данных в таблицу cart
 INSERT INTO cart (id, user_id, card_id, quantity) VALUES
