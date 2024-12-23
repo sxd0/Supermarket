334a9b0d9263f9d06dbe86d20544a1820509b530
xAsssenov <aks.kirill32@gmail.com>
2024-12-22 16:43:23 -0800

    fix: for macOS

--- backend/app/config.py
+++ backend/app/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
+
     TEST_DB_HOST: str
     TEST_DB_PORT: int
     TEST_DB_USER: str
@@ -34,9 +34,12 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str
-    
-
 
     model_config = ConfigDict(env_file=".env")
+
+    @model_validator(mode='before')
+    def validate_environment(cls, values):
+        print("Loaded environment variables:", values)
+        return values
 
 settings = Settings()
